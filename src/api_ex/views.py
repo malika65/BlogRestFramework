df4998a91a0ba8be950d02c19c8f87ae66198456
malika65 <malika.satimbaeva@iaau.edu.kg>
2021-01-14 18:24:44 +0600

    Tag view detail all posts

--- src/api_ex/views.py
+++ src/api_ex/views.py
@@ -68,10 +68,6 @@ class PostDeleteView(DestroyAPIView):
     lookup_url_kwarg = 'post_id'
     queryset = Post.objects.all()
 
-# class UpdateName(generics.UpdateAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostListSerializer
-#     permission_classes = (permissions.IsAuthenticated,)
 
 def post_view(request):
     posts = Post.objects.all().order_by("-id")
@@ -88,7 +84,7 @@ def post_detail_view(request,post_id):
 
 def tag_detail_view(request, tag_id):
     posts = Post.objects.filter(tags__id = tag_id)
-    return render(request, "post_detail.html", context = {"posts":posts})  
+    return render(request, "post.html", context = {"posts":posts})  
 
 def post_create(request):
     if request.method == "POST":
@@ -101,50 +97,3 @@ def post_create(request):
         form = PostForm()
     return render(request, 'post_create.html', {'form': form})
 
-# def deletePost(request,post_id):
-#     Post.objects.filter(id = post_id).delete()
-#     return JsonResponse(data = {"success":True} )
-
-# def deleteTag(request,tag_id):
-#     Tag.objects.filter(id = tag_id).delete()
-#     return JsonResponse(data = {"success":True} )
-
-# @api_view(['GET'])
-# def all_posts(request):
-#     posts = Post.objects.all()
-#     # posts = [{'title':i.title, 'body':i.body} for i in posts]
-#     ser = PostListSerializer(posts , many = True)
-    
-#     return Response(data = ser.data)
-# @api_view(['GET'])
-# def all_tags(request):
-#     tags = Tag.objects.all()
-#     ser = TagListSerializer(tags , many = True)
-    # return Response(data = ser.data)
-# @api_view(['POST'])
-# def post_create(request):
-#     ser = PostCreateSerializer(data = request.data)
-#     ser.is_valid(raise_exception=True)
-#     Post.objects.create(**ser.data)
-#     return Response(data = {"success":True,"message":"Post created"})
-
-
-# @api_view(['POST'])
-# def tag_create(request):
-#     ser = TagCreateSerializer(data = request.data)
-#     ser.is_valid(raise_exception=True)
-#     Tag.objects.create(**ser.data)
-#     return Response(data = {"success":True,"message":"Tag created"})
-
-# @api_view(['GET'])
-# def post_details(request, post_id):
-#     posts = Post.objects.get(id = post_id)
-#     ser = PostDetailSerializer(posts)
-#     return Response(data = ser.data)
-
-
-# @api_view(['GET'])
-# def tag_details(request, tag_id):
-#     tags = Tag.objects.get(id = tag_id)
-#     ser = TagDetailSerializer(tags)
-#     return Response(data = ser.data)
